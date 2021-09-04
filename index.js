97b088dcffc6098d7d80c28c03714dd8938bb485
Julio Soto <juliosotodev@gmail.com>
2021-09-04 04:52:59 -0600

    canvas and front interface setup

--- index.js
+++
@@ -1,6 +0,0 @@
-import { color, add } from './module.js';
-
-console.log(color); // "green"
-console.log(add(20, 40)); // 60
-
-console.log('hello friend')
---
+++ main.js
@@ -0,0 +1,6 @@
+var C = document.getElementById('C');
+var ctx = C.getContext("2d");
+var container = document.getElementById('container');
+window.addEventListener('keydown',readKey,false);
+
+
